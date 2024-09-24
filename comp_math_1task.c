17354974ed231646f6787f6a19a5d8e0f86a520d
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2024-09-25 01:25:14 +0600

    add random

--- comp_math_1task.c
+++ comp_math_1task.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <cmath>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
+
+    srand(time(0));
+    
     int i = 1;
     int n = 10;
     double b = 3 * M_PI / 2;
     double a = M_PI;
     double ans = 0;
     while(i <= n){
-        r = (b - a)
+        double random_double = (double)rand() / RAND_MAX;
+        double r = (b - a) * random_double;
     }
 }
