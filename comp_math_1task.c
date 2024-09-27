f1f34d1b857a7e16540ae9ce8f43f9de53096a29
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2024-09-28 00:06:06 +0600

    change for MC method

--- comp_math_1task.c
+++ comp_math_1task.c
@@ -4,19 +4,18 @@
 #include <time.h>
 
 int main() {
-
     srand(time(0));
     
     int i = 1;
-    int n = 10;
+    int n = 1000;
     double b = 3 * M_PI / 2;
     double a = M_PI;
     double ans = 0;
     while(i <= n){
         double random_double = (double)rand() / RAND_MAX;
-        double r = (b - a) * random_double;
+        double r = ((b - a) * random_double) + a;
         double ansforR = sin(r);
-        ans = ans + ansforR * r;
+        ans = ans + ansforR * (b - a);
         double ave = ans / i;
         printf("\n%f", ave);
         i++;
